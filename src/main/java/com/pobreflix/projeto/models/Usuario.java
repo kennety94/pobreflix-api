513cd4c5ec7eb39b3a0f0750a0e762c3d9d3ce0f
Kennety <kennetyandersson@gmail.com>
2019-11-04 22:49:40 -0300

    Primeiro commit com as configurações iniciais

---
+++ src/main/java/com/pobreflix/projeto/models/Usuario.java
@@ -0,0 +1,48 @@
+package com.pobreflix.projeto.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_USUARIO")
+public class Usuario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String nome;
+	private String email;
+	private String senha;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
